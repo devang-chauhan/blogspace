40f43c77e790b26b5f8c55761d97d6387ba3a53a
Devang <devngc@gmail.com>
2022-09-22 21:44:29 -0400

    fix(*): working version of the app

--- js/main.js
+++ js/main.js
@@ -13,15 +13,27 @@ function injectPosts(posts) {
 }
 
 
-
-
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(data => {
         const posts = data.slice(0, 5);
         injectPosts(posts);
     });
-    
+
+
+
+function addPosts(data) {
+    const html = `
+    <div class="post">
+        <h3>${data.title}</h3>
+        <p>${data.body}</p>
+    </div>`;
+
+    document.getElementById("blogs").innerHTML = `
+    ${html}
+    ${document.getElementById("blogs").innerHTML}
+    `;
+}
 
 
 
@@ -30,10 +42,20 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
     const title = document.getElementById("title").value;
     const body = document.getElementById("body").value;
     const data = {
-        "title": title,
-        "body": body
+        title: title,
+        body: body
     };
-    console.log(data);
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => addPosts(data));
 });
 
 
